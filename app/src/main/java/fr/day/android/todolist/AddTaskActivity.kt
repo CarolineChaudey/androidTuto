8f44ba8b88d59614a6c414bf1e182d010d22453a
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-11-27 22:15:10 +0100

    creation form design

--- app/src/main/java/fr/day/android/todolist/AddTaskActivity.kt
+++ app/src/main/java/fr/day/android/todolist/AddTaskActivity.kt
@@ -1,9 +1,7 @@
 package fr.day.android.todolist
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-
 import kotlinx.android.synthetic.main.activity_add_task.*
 
 class AddTaskActivity : AppCompatActivity() {
@@ -13,10 +11,6 @@ class AddTaskActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_task)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
