2016a824584c3c463e7ed2dfc1bbeae47ef67d3a
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-11-27 22:36:18 +0100

    creation confirmation

--- app/src/main/java/fr/day/android/todolist/AddTaskActivity.kt
+++ app/src/main/java/fr/day/android/todolist/AddTaskActivity.kt
@@ -3,6 +3,7 @@ package fr.day.android.todolist
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add_task.*
+import kotlinx.android.synthetic.main.content_add_task.*
 
 class AddTaskActivity : AppCompatActivity() {
 
@@ -12,6 +13,13 @@ class AddTaskActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        add_button.setOnClickListener { view ->
+            val title = task_title_field.text.toString()
+            val description = task_description_field.text.toString()
+            val task = Task(title, description)
+            finish()
+        }
     }
 
 }
---
+++ app/src/main/java/fr/day/android/todolist/Task.kt
@@ -0,0 +1,6 @@
+package fr.day.android.todolist
+
+/**
+ * Created by caroline on 27/11/17.
+ */
+data class Task(var title: String, var description: String = "")
