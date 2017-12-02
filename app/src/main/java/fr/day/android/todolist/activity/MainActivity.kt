7f94362b0b0def9cbe89594c72ebb10c3ff5cdc2
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-12-02 16:00:53 +0100

    todo list

--- app/src/main/java/fr/day/android/todolist/activity/MainActivity.kt
+++ app/src/main/java/fr/day/android/todolist/activity/MainActivity.kt
@@ -1,15 +1,12 @@
 package fr.day.android.todolist
 
 import android.content.Intent
-import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.Menu
-import android.view.MenuItem
-
+import fr.day.android.todolist.adapters.TaskAdapter
 import kotlinx.android.synthetic.main.activity_list.*
+import kotlinx.android.synthetic.main.content_list.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,21 +17,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_list)
         setSupportActionBar(toolbar)
 
-
+        //val dataList = listOf(Task(1, "Faire le ménage", "Super crados"), Task(2, "Coder le tuto", "Ultra urgent"),
+                //Task(3, "Faire du sport", "Je suis fatigué"), Task(4, "Réviser l'électro", "Je n'ai toujours rien compris à ce cours"))
+        /*
         SQLiteManager.getInstance(this)?.let {
-            listOf(Task(1, "Faire le ménage", "Super crados"), Task(2, "Coder le tuto", "Ultra urgent"),
-                    Task(3, "Faire du sport", "Je suis fatigué"), Task(4, "Réviser l'électro", "Je n'ai toujours rien compris à ce cours"))
-                    .forEach { task ->
+            dataList.forEach { task ->
                         it.add(task)
-                    }
+            }
         }
-
+    */
+        val data = SQLiteManager.getInstance(this)?.getAll()
+        if (data != null) taskListView.adapter = TaskAdapter(this, data)
 
         fab.setOnClickListener {
 
-            //val toAddTask = Intent(this, AddTaskActivity::class.java)
-            //startActivity(toAddTask)
-
+            val toAddTask = Intent(this, AddTaskActivity::class.java)
+            startActivity(toAddTask)
+            /*
             SQLiteManager.getInstance(this)?.let {
 
                 SQLiteManager.getInstance(this)?.let {
@@ -46,6 +45,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
             }
+            */
         }
     }
 
